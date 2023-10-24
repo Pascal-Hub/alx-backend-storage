443b5a705021297b7222e1daab1786fceb1d239b
Pascal-Hub <sequencepascal@gmail.com>
2023-10-24 09:52:53 +0000

    redis basic advance

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,55 +1,36 @@
 #!/usr/bin/env python3
-''' Task 5 '''
-
-
-import redis
+"""
+web cache and tracker
+"""
 import requests
-import time
+import redis
 from functools import wraps
 
-
-redis_client = redis.Redis()
-
-
-def track_url_access_count(url):
-    ''' Decorator to track URL access count in Redis.
-    '''
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            redis_client.incr(f"count:{url}")
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
+store = redis.Redis()
 
 
-@track_url_access_count("http://slowly.robertomurray.co.uk")
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
+
+
+@count_url_access
 def get_page(url: str) -> str:
-    ''' get HTML content of a URL and cache it for 10 seconds.
-    '''
-    cache_content = redis_client.get(url)
-    if cache_content:
-        return cache_content.decode("utf-8")
-
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        content = response.text
-        redis_client.setex(url, 10, content)
-        return content
-
-
-    return ""
-
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    for _ in range(5):
-        content = get_page(url)
-        print(content[:100])
-        time.sleep(1)
-
-
-    access_count = redis_client.get(f"count:{url}")
-    access_count = int(access_count) if access_count is not None else 0
-    print(f"URL accessed {int(access_count)} time.")
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
